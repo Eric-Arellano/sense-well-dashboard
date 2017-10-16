a86744acdad434d4b6ce2066a9c1a3ea7a57e3c5
Eric Arellano <ericarellano@me.com>
2017-10-15 22:09:59 -0700

    add rudimentary titles and dates to metric summaries

--- src/components/Widgets/MetricSummary.js
+++ src/components/Widgets/MetricSummary.js
@@ -2,35 +2,41 @@
 import React from 'react';
 import classNames from 'classnames';
 import {mapToCssModules} from 'reactstrap/lib/utils';
-import {Card, CardBody, CardFooter} from 'reactstrap';
 
 type Props = {
   className: string,
   cssModule: any,
+  name: string,
   currentValue: number,
+  date: Date,
   average: number,
   threshold: (number) => boolean,
   thresholdText: string
 };
 
 const MetricSummary = (props: Props) => {
-  const {className, cssModule, currentValue, average, threshold, thresholdText, ...attributes} = props;
+  const {className, cssModule, name, currentValue, date, average, threshold, thresholdText, ...attributes} = props;
   const okay = threshold(currentValue) ? 'okay' : 'bad';
+  const daysDiff = Math.round((new Date() - date)/(1000*60*60*24));
   const classes = mapToCssModules(classNames("metric-box", className, okay), cssModule);
 
   return (
-    <div className={classes}>
-      <span className={"mainText"}>{currentValue}</span>
-      <ul>
-        <li>
-          <strong>{average}</strong>
-          <span>average</span>
-        </li>
-        <li>
-          <strong>{thresholdText}</strong>
-          <span>safe values</span>
-        </li>
-      </ul>
+    <div>
+      <div className={classes}>
+        <span className={"mainText"}>{currentValue}</span>
+        <ul>
+          <li>
+            <strong>{average}</strong>
+            <span>average</span>
+          </li>
+          <li>
+            <strong>{thresholdText}</strong>
+            <span>safe values</span>
+          </li>
+        </ul>
+        <span>Last updated {daysDiff} days ago.</span>
+      </div>
+      <h3>{name}</h3>
     </div>
   )
 };
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -9,13 +9,16 @@ class Dashboard extends Component {
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="6" lg="3">
-            <MetricSummary currentValue={43} average={74} threshold={(value) => value > 20} thresholdText={"> 20"} />
+            <MetricSummary name="Water flow" date={new Date(2017, 4, 11)} currentValue={43} average={74}
+                           threshold={(value) => value > 20} thresholdText={"> 20"} />
           </Col>
           <Col xs="12" sm="6" lg="3">
-            <MetricSummary currentValue={65} average={32} threshold={(value) => value < 33} thresholdText={"< 33%"} />
+            <MetricSummary name="Turbidity" date={new Date(2017, 8, 17)} currentValue={65} average={32}
+                           threshold={(value) => value < 33} thresholdText={"< 33%"} />
           </Col>
           <Col xs="12" sm="6" lg="3">
-            <MetricSummary currentValue={0.41} average={0.84} threshold={(value) => value < 0.6} thresholdText={"< 0.6"} />
+            <MetricSummary name="Salinity" date={new Date(2017, 9, 14)} currentValue={0.41} average={0.84}
+                           threshold={(value) => value < 0.6} thresholdText={"< 0.6"} />
           </Col>
         </Row>
       </div>
