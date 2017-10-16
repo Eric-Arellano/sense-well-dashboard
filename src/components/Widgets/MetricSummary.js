e6d9545f57fcbc5f3ed7f6d4fc3675402494f2bd
Eric Arellano <ericarellano@me.com>
2017-10-15 19:57:36 -0700

    rename file & add threshold text

--- src/components/Widgets/MetricSummary.js
+++ src/components/Widgets/MetricSummary.js
@@ -1,57 +1,37 @@
+// @flow
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import {mapToCssModules} from 'reactstrap/lib/utils';
 
-// SEE WIDGET 3 for basis
-
-const propTypes = {
-  className: PropTypes.string,
-  cssModule: PropTypes.object,
-  name: PropTypes.string.isRequired,
-  units: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date),
-  currentValue: PropTypes.number,
-  average: PropTypes.number,
-  threshold: PropTypes.number
+type Props = {
+  className: string,
+  cssModule: any,
+  currentValue: number,
+  average: number,
+  threshold: (number) => boolean,
+  thresholdText: string
 };
 
-const defaultProps = {
+const MetricSummary = (props: Props) => {
+  const {className, cssModule, currentValue, average, threshold, thresholdText, ...attributes} = props;
+  const okay = threshold(currentValue) ? 'okay' : 'bad';
+  const classes = mapToCssModules(classNames("metric-box", className, okay), cssModule);
 
+  return (
+    <div className={classes}>
+      <span className={"mainText"}>{currentValue}</span>
+      <ul>
+        <li>
+          <strong>{average}</strong>
+          <span>average</span>
+        </li>
+        <li>
+          <strong>{thresholdText}</strong>
+          <span>safe values</span>
+        </li>
+      </ul>
+    </div>
+  )
 };
-
-class MetricSummary extends Component {
-  render() {
-    const {className, cssModule, name, units, date, currentValue, average, threshold,
-      ...attributes} = this.props;
-
-    const okay = currentValue > threshold ? "okay" : "bad";
-
-    // const classes = mapToCssModules(classNames("social-box", className, okay), cssModule);
-
-    return (
-      /*<div className={classes}>*/
-    <div >
-        <ul>
-          <li>
-            <strong>{average}</strong>
-            <span>average</span>
-          </li>
-          <li>
-            <strong>{vals[2]}</strong>
-            <span>threshold</span>
-          </li>
-        </ul>
-      </div>
-    )
-  }
-}
-
-Widget03.propTypes = propTypes;
-Widget03.defaultProps = defaultProps;
 
 export default MetricSummary;
-
-
-// example call of Widget3:
-// <Widget03 dataBox={() => ({variant: "facebook", friends : "89k", feeds: "459"})}/>
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MetricSummary2 from "../../components/Widgets/MetricSummary2";
+import MetricSummary from "../../components/Widgets/MetricSummary";
 
 class Dashboard extends Component {
 
@@ -7,7 +7,7 @@ class Dashboard extends Component {
     return (
       <div className="animated fadeIn">
         Hello World
-        <MetricSummary2 currentValue={43} average={74} threshold={(value) => value > 20} />
+        <MetricSummary currentValue={43} average={74} threshold={(value) => value > 20} thresholdText={"> 20"} />
       </div>
     )
   }
