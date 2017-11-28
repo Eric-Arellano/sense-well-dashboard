c0e19074a78c5b37dde67e1f3ef09cf70125fa8d
Eric Arellano <ericarellano@me.com>
2017-11-27 18:35:18 -0700

    add api skeleton

--- src/containers/Dashboard/DashboardContainer.js
+++ src/containers/Dashboard/DashboardContainer.js
@@ -2,25 +2,48 @@
 import React, { Component } from 'react'
 import { Dashboard } from 'components'
 import type { MetricSummary } from '../../flow/types'
+import { getMetricSummaries } from '../../utils/api'
 import { mockMetricSummaries } from 'data/mock'
 
-type Props = { }
+type Props = {
+  community: string
+}
 
 type State = {
+  isLoading: boolean,
+  isError: boolean,
   metricSummaries: Array<MetricSummary>
 }
 
 class DashboardContainer extends Component<Props, State> {
 
   state = {
-    metricSummaries: mockMetricSummaries()
+    isLoading: true,
+    isError: false,
+    metricSummaries: []
   }
 
-  render() {
-    const { metricSummaries } = this.state
+  componentDidMount () {
+    getMetricSummaries(this.props.community)
+      .then((data) => {
+        this.setState({
+          metricSummaries: data.metricSummaries,
+          isLoading: false,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          metricSummaries: mockMetricSummaries(), // TODO: just for demo
+          isLoading: false,
+          isError: true
+        })
+      })
+  }
+
+  render () {
     return (
-        <Dashboard metricSummaries={metricSummaries}/>
-      )
+      <Dashboard {...this.state} />
+    )
   }
 }
 
