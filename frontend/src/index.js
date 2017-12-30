fa48db9cf742ca70b0f55ab2f2f690ef60037a18
Eric Arellano <ericarellano@me.com>
2017-12-30 11:30:26 -0700

    add resets css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
-import 'normalize.css'
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'containers'
+import './resets.css'
 
 render(<AppContainer />, document.getElementById('root'))
