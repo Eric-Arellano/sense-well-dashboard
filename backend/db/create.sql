fb388f099bcf08c73c2c10cd9e23c70a9ccfaa05
Eric Arellano <ericarellano@me.com>
2018-01-01 12:28:08 -0700

    add metric summaries to schema

--- backend/db/create.sql
+++ backend/db/create.sql
@@ -1,23 +1,39 @@
 CREATE TABLE daily_conditions (
-  date            DATE                                  NOT NULL,
-  location_id     INT REFERENCES location (location_id) NOT NULL,
-  flow_count      INT                                   NOT NULL,
-  flow_sum        DOUBLE PRECISION                      NOT NULL,
-  salinity_count  INT                                   NOT NULL,
-  salinity_sum    DOUBLE PRECISION                      NOT NULL,
-  turbidity_count INT                                   NOT NULL,
-  turbidity_sum   DOUBLE PRECISION                      NOT NULL,
-  PRIMARY KEY (date, location_id)
+  date            DATE                                    NOT NULL,
+  community_id    INT REFERENCES community (community_id) NOT NULL,
+  flow_count      INT                                     NOT NULL,
+  flow_sum        DOUBLE PRECISION,
+  salinity_count  INT                                     NOT NULL,
+  salinity_sum    DOUBLE PRECISION,
+  turbidity_count INT                                     NOT NULL,
+  turbidity_sum   DOUBLE PRECISION,
+  PRIMARY KEY (date, community_id)
 );
 
 SELECT create_hypertable('daily_conditions', 'time');
 
-CREATE TABLE location (
-  location_id  INT PRIMARY KEY,
+CREATE TABLE community (
+  community_id INT PRIMARY KEY,
   name         TEXT  NOT NULL,
   lat_long     POINT NOT NULL,
   size         INT,
   install_date DATE,
   update_date  DATE
 );
+
+CREATE TABLE flow (
+  most_recent_daily_value DOUBLE PRECISION,
+  daily_average           DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE salinity (
+  most_recent_daily_value DOUBLE PRECISION,
+  daily_average           DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE turbidity (
+  most_recent_daily_value DOUBLE PRECISION,
+  daily_average           DOUBLE PRECISION NOT NULL
+);
+
 
