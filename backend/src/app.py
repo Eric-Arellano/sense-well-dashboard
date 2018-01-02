b8488e08db0530680702b710d41f6eb8bad5eae0
Eric Arellano <ericarellano@me.com>
2018-01-01 23:13:21 -0700

    fix app to accept time series post request

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request, send_from_directory, abort
 
 from src.mock import mock_time_series
+from src import parser
 
 app = Flask(__name__,
             static_folder="../../frontend/build/static",
@@ -32,14 +33,16 @@ def time_series(community: str):
     return return_json(result)
 
 
-@app.route('/api/time_series/', methods=['POST'])
+@app.route('/api/time_series', methods=['POST'])
 def time_series_post():
-    community_name = request.form['body']['community_name']
-    datapoints = request.form['body']['datapoints']
-    # TODO: parse datapoints, including into each logical day
+    req_data = request.get_json()
+    community_name = req_data['body']['community_name']
+    datapoints = req_data['body']['datapoints']
+    # day_aggregates = parser.parse_datapoint_set(datapoints)
     # TODO: submit datapoints as row associated with that community and day
     # TODO: also update running averages and current value for metric summaries
-    return community_name + datapoints
+    # return community_name + datapoints
+    return community_name
 
 
 def return_json(result):
