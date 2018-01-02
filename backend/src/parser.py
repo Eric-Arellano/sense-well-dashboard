aa2cbf1119705d0249563ce9905d66dba338d5db
Eric Arellano <ericarellano@me.com>
2018-01-01 20:23:19 -0700

    add parser using pandas

--- backend/src/parser.py
+++ backend/src/parser.py
@@ -1,21 +1,41 @@
 from typing import List, Dict
 
+import pandas
+
 Datapoint = Dict[str, float]
 DailyConditions = Dict[str, float]
 
 
 def parse_datapoint_set(datapoints: List[Datapoint]) -> List[DailyConditions]:
-    datapoints_by_day = split_into_dates(datapoints)
-    daily_conditions: List[DailyConditions] = []
-    for day in datapoints_by_day:
-        daily_condition = parse_day_into_daily_conditions(day)
-        daily_conditions.append(daily_condition)
-    return daily_conditions
+    data_frame = _convert_to_pandas(datapoints)
+    grouped = _group_by_day(data_frame)
+    aggregated = _get_count_and_sum_per_day(grouped)
+    return _parse_into_daily_conditions(aggregated)
 
 
-def split_into_dates(datapoints: List[Datapoint]) -> List[List[Datapoint]]:
-    raise NotImplementedError
+def _convert_to_pandas(datapoints: List[Datapoint]) -> pandas.DataFrame:
+    raw_dates = [d['timestamp'] for d in datapoints]
+    dates = pandas.to_datetime(raw_dates, unit='s')
+    return pandas.DataFrame(datapoints, index=dates, columns=['flow', 'salinity', 'turbidity'])
 
 
-def parse_day_into_daily_conditions(datapoints: List[Datapoint]) -> DailyConditions:
-    raise NotImplementedError
+def _group_by_day(data_frame: pandas.DataFrame):
+    return data_frame.groupby(pandas.Grouper(freq='D'))
+
+
+def _get_count_and_sum_per_day(groups) -> pandas.DataFrame:
+    count = groups.count()
+    count.columns = ['flow_count', 'salinity_count', 'turbidity_count']
+    sum_ = groups.sum()
+    sum_.columns = ['flow_sum', 'salinity_sum', 'turbidity_sum']
+    join = count.join(sum_, how='outer')
+    return join.sort_index(axis=1)  # sort column names alphabetically
+
+
+def _parse_into_daily_conditions(aggregated: pandas.DataFrame) -> List[DailyConditions]:
+    r = aggregated.to_dict(orient='index')
+    print(r)
+    r2 = dict((key.date(), value) for (key, value) in r)
+    print(r2)
+    raise NotImplementedError('Figure out how to convert to List of Dicts, with keys "day", "x_count", and "x_sum"')
+    # return [{'day': d, 'flow_count': d['flow_count']} for d in r]
--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -1,5 +1,8 @@
 import unittest
-import mypackage.parser
+
+import numpy
+
+import src.parser as parser
 
 
 class TestParser(unittest.TestCase):
@@ -31,9 +34,36 @@ class TestParser(unittest.TestCase):
             },
             {
                 "timestamp": 1514851341,
-                "flow": 27.6
+                "flow": 27.6,
+                "turbidity": 1.6351
             }
         ]
+        cls.data_frame = parser._convert_to_pandas(cls.datapoints)
+        cls.group = parser._group_by_day(cls.data_frame)
+        cls.aggregated = parser._get_count_and_sum_per_day(cls.group)
+
+    @unittest.skip("Not sure why these dicts aren't evaluating to the same?")
+    def test_convert_to_pandas(self):
+        expected = {'timestamp': {0: 1514833341, 1: 1514836941},
+                    'flow': {0: 37.2, 1: 29.9},
+                    'salinity': {0: 19.23, 1: numpy.NaN},
+                    'turbidity': {0: 0.5476, 1: numpy.NaN}}
+        data = self.datapoints[:2]
+        result = parser._convert_to_pandas(data)
+        self.assertDictEqual(result.to_dict(), expected)
 
     def test_split_days(self):
-        pass
+        result = parser._group_by_day(self.data_frame).count()
+        day1 = result.loc['2018-01-01']
+        day2 = result.loc['2018-01-02']
+        self.assertEqual(len(result), 2)
+        self.assertEqual(day1["flow"].item(), 5)
+        self.assertEqual(day2["flow"].item(), 1)
+
+    @unittest.skip("Not sure how to test")
+    def test_get_count_and_sum_by_day(self):
+        result = parser._get_count_and_sum_per_day(self.group)
+
+    def test_parse_into_daily_conditions(self):
+        result = parser._parse_into_daily_conditions(self.aggregated)
+        print(result)
